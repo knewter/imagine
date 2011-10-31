a29b5f59bc65e3aadd1c00b9c1aebce803f84238
Bram Swenson <bram@craniumisajar.com>
2011-10-30 18:24:55 -0700

    [Models] more specs

--- spec/app/models/album_spec.rb
+++ spec/app/models/album_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Imagine::Album do
   it { should validate_presence_of(:name) }
+  it { should have_many(:images) }
+  if Imagine.orm == :mongoid
+    context "with Mongoid" do
+      it { should have_field(:name).of_type(String) }
+      it { should be_timestamped_document }
+    end
+  end
 end
--- spec/app/models/image_spec.rb
+++ spec/app/models/image_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe Imagine::Image do
   it { should validate_presence_of(:file) }
+  it { should belong_to(:album) }
+  if Imagine.orm == :mongoid
+    context "with Mongoid" do
+      it { should have_field(:file_name).of_type(String) }
+      it { should have_field(:file_uid).of_type(String) }
+      it { should be_timestamped_document }
+    end
+  end
 end
